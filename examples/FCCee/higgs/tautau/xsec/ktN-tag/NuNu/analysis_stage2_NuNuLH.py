8aaa0a310cb219a091651c94fb91825c315bffe6
Sofia Giappichini <sofia.giappichini@cern.ch>
2024-12-03 10:18:51 +0100

    new xsec analyses and lxplus paths

--- examples/FCCee/higgs/tautau/xsec/ktN-tag/NuNu/analysis_stage2_NuNuLH.py
+++ examples/FCCee/higgs/tautau/xsec/ktN-tag/NuNu/analysis_stage2_NuNuLH.py
@@ -2,108 +2,112 @@ import os, copy # tagging
 import ROOT
 
 #Mandatory: List of processes
-processList_ = {
-    'wzp6_ee_nunuH_Htautau_ecm240': {'chunks':1},
-    'wzp6_ee_eeH_Htautau_ecm240': {'chunks':1},
-    'wzp6_ee_mumuH_Htautau_ecm240': {'chunks':1},
-    'wzp6_ee_bbH_Htautau_ecm240': {'chunks':1},
-    'wzp6_ee_ccH_Htautau_ecm240': {'chunks':1},
-    'wzp6_ee_ssH_Htautau_ecm240': {'chunks':1},
-    'wzp6_ee_qqH_Htautau_ecm240': {'chunks':1},
-}
 processList = {
-    'p8_ee_WW_ecm240':{'chunks':100},
-    'p8_ee_Zqq_ecm240':{'chunks':100},
-    'p8_ee_ZZ_ecm240':{'chunks':100},
+
+    'p8_ee_WW_ecm240':{'chunks':3740},
+    'p8_ee_Zqq_ecm240':{'chunks':1007},
+    'p8_ee_ZZ_ecm240':{'chunks':1000},
     
-    'wzp6_ee_tautau_ecm240':{'chunks':100},
-    'wzp6_ee_mumu_ecm240':{'chunks':100},
-    'wzp6_ee_ee_Mee_30_150_ecm240':{'chunks':100},
+    'wzp6_ee_tautau_ecm240':{'chunks':1000},
+    'wzp6_ee_mumu_ecm240':{'chunks':1000},
+    'wzp6_ee_ee_Mee_30_150_ecm240':{'chunks':1000},
 
-    'wzp6_ee_tautauH_Htautau_ecm240': {'chunks':10},
-    'wzp6_ee_tautauH_Hbb_ecm240': {'chunks':10},
-    'wzp6_ee_tautauH_Hcc_ecm240': {'chunks':10},
-    'wzp6_ee_tautauH_Hss_ecm240': {'chunks':10},
-    'wzp6_ee_tautauH_Hgg_ecm240': {'chunks':10},
-    'wzp6_ee_tautauH_HWW_ecm240': {'chunks':10},
-    'wzp6_ee_tautauH_HZZ_ecm240': {'chunks':10},
+    'wzp6_ee_tautauH_Htautau_ecm240': {'chunks':100},
+    'wzp6_ee_tautauH_Hbb_ecm240': {'chunks':100},
+    'wzp6_ee_tautauH_Hcc_ecm240': {'chunks':100},
+    'wzp6_ee_tautauH_Hss_ecm240': {'chunks':100},
+    'wzp6_ee_tautauH_Hgg_ecm240': {'chunks':100},
+    'wzp6_ee_tautauH_HWW_ecm240': {'chunks':100},
+    'wzp6_ee_tautauH_HZZ_ecm240': {'chunks':100},
 
-    'wzp6_egamma_eZ_Zmumu_ecm240': {'chunks':10},
-    'wzp6_egamma_eZ_Zee_ecm240': {'chunks':10},
-    'wzp6_gammae_eZ_Zmumu_ecm240': {'chunks':10},
-    'wzp6_gammae_eZ_Zee_ecm240': {'chunks':10},
+    'wzp6_egamma_eZ_Zmumu_ecm240': {'chunks':1000},
+    'wzp6_egamma_eZ_Zee_ecm240': {'chunks':1000},
+    'wzp6_gammae_eZ_Zmumu_ecm240': {'chunks':1000},
+    'wzp6_gammae_eZ_Zee_ecm240': {'chunks':1000},
 
-    'wzp6_gaga_tautau_60_ecm240': {'chunks':100},
-    'wzp6_gaga_mumu_60_ecm240': {'chunks':100},
-    'wzp6_gaga_ee_60_ecm240': {'chunks':100},
+    'wzp6_gaga_tautau_60_ecm240': {'chunks':1000},
+    'wzp6_gaga_mumu_60_ecm240': {'chunks':1000},
+    'wzp6_gaga_ee_60_ecm240': {'chunks':1000},
 
-    'wzp6_ee_nuenueZ_ecm240': {'chunks':10},
+    'wzp6_ee_nuenueZ_ecm240': {'chunks':1000},
+    'wzp6_ee_nunuH_Htautau_ecm240': {'chunks':100},
+    'wzp6_ee_nunuH_Hbb_ecm240': {'chunks':100},
+    'wzp6_ee_nunuH_Hcc_ecm240': {'chunks':100},
+    'wzp6_ee_nunuH_Hss_ecm240': {'chunks':100},
+    'wzp6_ee_nunuH_Hgg_ecm240': {'chunks':100},
+    'wzp6_ee_nunuH_HWW_ecm240': {'chunks':100},
+    'wzp6_ee_nunuH_HZZ_ecm240': {'chunks':100},
 
-    'wzp6_ee_nunuH_Htautau_ecm240': {'chunks':10},
-    'wzp6_ee_nunuH_Hbb_ecm240': {'chunks':10},
-    'wzp6_ee_nunuH_Hcc_ecm240': {'chunks':10},
-    'wzp6_ee_nunuH_Hss_ecm240': {'chunks':10},
-    'wzp6_ee_nunuH_Hgg_ecm240': {'chunks':10},
-    'wzp6_ee_nunuH_HWW_ecm240': {'chunks':10},
-    'wzp6_ee_nunuH_HZZ_ecm240': {'chunks':10},
+    'wzp6_ee_eeH_Htautau_ecm240': {'chunks':100},
+    'wzp6_ee_eeH_Hbb_ecm240': {'chunks':100},
+    'wzp6_ee_eeH_Hcc_ecm240': {'chunks':100},
+    'wzp6_ee_eeH_Hss_ecm240': {'chunks':100},
+    'wzp6_ee_eeH_Hgg_ecm240': {'chunks':100},
+    'wzp6_ee_eeH_HWW_ecm240': {'chunks':100},
+    'wzp6_ee_eeH_HZZ_ecm240': {'chunks':100},
 
-    'wzp6_ee_eeH_Htautau_ecm240': {'chunks':10},
-    'wzp6_ee_eeH_Hbb_ecm240': {'chunks':10},
-    'wzp6_ee_eeH_Hcc_ecm240': {'chunks':10},
-    'wzp6_ee_eeH_Hss_ecm240': {'chunks':10},
-    'wzp6_ee_eeH_Hgg_ecm240': {'chunks':10},
-    'wzp6_ee_eeH_HWW_ecm240': {'chunks':10},
-    'wzp6_ee_eeH_HZZ_ecm240': {'chunks':10},
+    'wzp6_ee_mumuH_Htautau_ecm240': {'chunks':100},
+    'wzp6_ee_mumuH_Hbb_ecm240': {'chunks':100},
+    'wzp6_ee_mumuH_Hcc_ecm240': {'chunks':100},
+    'wzp6_ee_mumuH_Hss_ecm240': {'chunks':100},
+    'wzp6_ee_mumuH_Hgg_ecm240': {'chunks':100},
+    'wzp6_ee_mumuH_HWW_ecm240': {'chunks':100},
+    'wzp6_ee_mumuH_HZZ_ecm240': {'chunks':100},
 
-    'wzp6_ee_mumuH_Htautau_ecm240': {'chunks':10},
-    'wzp6_ee_mumuH_Hbb_ecm240': {'chunks':10},
-    'wzp6_ee_mumuH_Hcc_ecm240': {'chunks':10},
-    'wzp6_ee_mumuH_Hss_ecm240': {'chunks':10},
-    'wzp6_ee_mumuH_Hgg_ecm240': {'chunks':10},
-    'wzp6_ee_mumuH_HWW_ecm240': {'chunks':10},
-    'wzp6_ee_mumuH_HZZ_ecm240': {'chunks':10},
+    'wzp6_ee_bbH_Htautau_ecm240': {'chunks':100},
+    'wzp6_ee_bbH_Hbb_ecm240': {'chunks':100},
+    'wzp6_ee_bbH_Hcc_ecm240': {'chunks':100},
+    'wzp6_ee_bbH_Hss_ecm240': {'chunks':100},
+    'wzp6_ee_bbH_Hgg_ecm240': {'chunks':100},
+    'wzp6_ee_bbH_HWW_ecm240': {'chunks':100},
+    'wzp6_ee_bbH_HZZ_ecm240': {'chunks':100},
 
-    'wzp6_ee_bbH_Htautau_ecm240': {'chunks':10},
-    'wzp6_ee_bbH_Hbb_ecm240': {'chunks':10},
-    'wzp6_ee_bbH_Hcc_ecm240': {'chunks':10},
-    'wzp6_ee_bbH_Hss_ecm240': {'chunks':10},
-    'wzp6_ee_bbH_Hgg_ecm240': {'chunks':10},
-    'wzp6_ee_bbH_HWW_ecm240': {'chunks':10},
-    'wzp6_ee_bbH_HZZ_ecm240': {'chunks':10},
+    'wzp6_ee_ccH_Htautau_ecm240': {'chunks':100},
+    'wzp6_ee_ccH_Hbb_ecm240': {'chunks':100},
+    'wzp6_ee_ccH_Hcc_ecm240': {'chunks':100},
+    'wzp6_ee_ccH_Hss_ecm240': {'chunks':100},
+    'wzp6_ee_ccH_Hgg_ecm240': {'chunks':100},
+    'wzp6_ee_ccH_HWW_ecm240': {'chunks':100},
+    'wzp6_ee_ccH_HZZ_ecm240': {'chunks':100},
 
-    'wzp6_ee_ccH_Htautau_ecm240': {'chunks':10},
-    'wzp6_ee_ccH_Hbb_ecm240': {'chunks':10},
-    'wzp6_ee_ccH_Hcc_ecm240': {'chunks':10},
-    'wzp6_ee_ccH_Hss_ecm240': {'chunks':10},
-    'wzp6_ee_ccH_Hgg_ecm240': {'chunks':10},
-    'wzp6_ee_ccH_HWW_ecm240': {'chunks':10},
-    'wzp6_ee_ccH_HZZ_ecm240': {'chunks':10},
+    'wzp6_ee_ssH_Htautau_ecm240': {'chunks':100},
+    'wzp6_ee_ssH_Hbb_ecm240': {'chunks':100},
+    'wzp6_ee_ssH_Hcc_ecm240': {'chunks':100},
+    'wzp6_ee_ssH_Hss_ecm240': {'chunks':100},
+    'wzp6_ee_ssH_Hgg_ecm240': {'chunks':100},
+    'wzp6_ee_ssH_HWW_ecm240': {'chunks':100},
+    'wzp6_ee_ssH_HZZ_ecm240': {'chunks':100},
 
-    'wzp6_ee_ssH_Htautau_ecm240': {'chunks':10},
-    'wzp6_ee_ssH_Hbb_ecm240': {'chunks':10},
-    'wzp6_ee_ssH_Hcc_ecm240': {'chunks':10},
-    'wzp6_ee_ssH_Hss_ecm240': {'chunks':10},
-    'wzp6_ee_ssH_Hgg_ecm240': {'chunks':10},
-    'wzp6_ee_ssH_HWW_ecm240': {'chunks':10},
-    'wzp6_ee_ssH_HZZ_ecm240': {'chunks':10},
-
-    'wzp6_ee_qqH_Htautau_ecm240': {'chunks':10},
-    'wzp6_ee_qqH_Hbb_ecm240': {'chunks':10},
-    'wzp6_ee_qqH_Hcc_ecm240': {'chunks':10},
-    'wzp6_ee_qqH_Hss_ecm240': {'chunks':10},
-    'wzp6_ee_qqH_Hgg_ecm240': {'chunks':10},
-    'wzp6_ee_qqH_HWW_ecm240': {'chunks':10},
-    'wzp6_ee_qqH_HZZ_ecm240': {'chunks':10},
-
+    'wzp6_ee_qqH_Htautau_ecm240': {'chunks':100},
+    'wzp6_ee_qqH_Hbb_ecm240': {'chunks':100},
+    'wzp6_ee_qqH_Hcc_ecm240': {'chunks':100},
+    'wzp6_ee_qqH_Hss_ecm240': {'chunks':100},
+    'wzp6_ee_qqH_Hgg_ecm240': {'chunks':100},
+    'wzp6_ee_qqH_HWW_ecm240': {'chunks':100},
+    'wzp6_ee_qqH_HZZ_ecm240': {'chunks':100},
 }
 
-inputDir = "/ceph/awiedl/FCCee/HiggsCP/stage1_241025/"
+#Mandatory: Production tag when running over EDM4Hep centrally produced events, this points to the yaml files for getting sample statistics
+inputDir = "/eos/experiment/fcc/ee/analyses_storage/Higgs_and_TOP/HiggsTauTau/"
 
-#Optional: output directory, default is local running directory
-outputDir   = "/ceph/awiedl/FCCee/HiggsCP/stage2_241025_cut/NuNu/LH/" 
+outputDir = "/eos/experiment/fcc/ee/analyses_storage/Higgs_and_TOP/HiggsTauTau/ktN-tag/stage2_241202/NuNu/LH"
 
 # additional/costom C++ functions, defined in header files (optional)
 includePaths = ["functions.h"]
+
+### necessary to run on HTCondor ###
+eosType = "eosuser"
+
+#Optional running on HTCondor, default is False
+runBatch = True
+
+nCPUS = 6
+
+#Optional batch queue name when running on HTCondor, default is workday
+batchQueue = "longlunch"
+
+#Optional computing account when running on HTCondor, default is group_u_FCC.local_gen
+compGroup = "group_u_FCC.local_gen"
 
 #Mandatory: RDFanalysis class where the use defines the operations on the TTree
 class RDFanalysis():
@@ -252,6 +256,8 @@ class RDFanalysis():
                 .Define("Tau_cos",      "Tau_scalar/(TauLead_p*TauSub_p)")
                 .Define("Tau_DEta",    "(TauLead_eta - TauSub_eta)")
                 .Define("Tau_DPhi",     "FCCAnalyses::ZHfunctions::deltaPhi(TauLead_phi, TauSub_phi)")
+
+                .Define("Visible_mass",    "RecoH_p4.M()")
 
                 #.Filter("RecoEmiss_e>100")
 
